812dda38e0ed074731b50bb7cbfc62cd606b151d
Deck <cristianjbc99@gmail.com>
2024-09-16 13:48:43 -0600

    finish de front

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import { useState } from 'react'
 import { useState } from "react";
 import "./App.css";
+import confetti from "canvas-confetti";
 
 const TURNS = {
   X: "x",
@@ -16,7 +17,7 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
 
   const handleClick = () => {
     updateBoard(index);
-  }
+  };
 
   return (
     <div className={className} onClick={handleClick}>
@@ -63,17 +64,30 @@ function App() {
       }
     }
     return null;
+  };
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setWinner(null);
+  }
+
+  const checkEndGame = (newBoard) => {
+    //Si todas las celdas estan llenas, se acaba el juego y es un empate
+    //every es un metodo que recorre un arreglo y retorna un booleano
+    return newBoard.every((square) => square !== null)
+
   }
 
   const updateBoard = (index) => {
     //Evitar que se pueda cambiar el valor de una celda
-    if ( board[index] || winner ) return
+    if (board[index] || winner) return;
 
     //actualiazar tablero
 
     //Los estados son inmutables, por lo que no se puede modificar directamente
     //spread y rest operator, en este caso se usa el spread operator para clonar el arreglo
-    const newBoard =[...board];
+    const newBoard = [...board];
     newBoard[index] = turn;
     setBoard(newBoard);
 
@@ -84,41 +98,57 @@ function App() {
     //Revisar si hay un ganador
     const newWinner = checkWinner(newBoard);
     if (newWinner) {
+      confetti();
       //En react, la renderizacion es asincrona, asi que saldra una alerta antes de que se actualice el estado
-      setWinner(newWinner); 
-
-    } //TODO: check if game is over
-  }
+      setWinner(newWinner);
+    } 
+    else if (checkEndGame(newBoard)){
+      setWinner(false);
+    }
+  };
 
   return (
     <main className="board">
       <h1>Tic tac toe</h1>
+
+      <button onClick={resetGame}>Reset del juego</button>
+
       <section className="game">
         {
           // map recorre el arreglo y retorna un nuevo arreglo
           // _ es el valor de cada elemento del arreglo  y index es la posicion del elemento
           // _ es un valor que no se va a utilizar
 
-          board.map((_, index) => (
-            <Square 
-              key={index} 
-              index={index}
-              updateBoard={updateBoard}
-            >
-              {board[index]}
+          board.map((square, index) => (
+            <Square key={index} index={index} updateBoard={updateBoard}>
+              {square}
             </Square>
           ))
         }
       </section>
 
       <section className="turn">
-        <Square isSelected={turn === TURNS.X}> 
-          {TURNS.X} 
-        </Square>
-        <Square isSelected={turn === TURNS.O} > 
-          {TURNS.O} 
-        </Square>
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
+
+      {
+        winner !== null && (
+          <section className="winner">
+            <div className="text">
+              <h2>{winner === false ? "Empate" : "Gano"}</h2>
+
+              <header className="win">
+                {winner && <Square>{winner}</Square>}
+              </header>
+
+              <footer>
+                <button onClick={resetGame}>Empezar de nuevo</button>
+              </footer>
+            </div>
+          </section>
+        )
+      }
     </main>
   );
 }
